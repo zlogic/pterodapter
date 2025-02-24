63a86e99a8af04595709e89ef13254f7f1fbb919
Dmitry Zolotukhin <zlogic@gmail.com>
2025-02-24 22:36:52 +0100

    Refactored IP decoder to extract more data.
    Log more details from IP packets.

--- src/ikev2/ip.rs
+++ src/ikev2/ip.rs
@@ -4,61 +4,227 @@ use std::{
     ops::RangeInclusive,
 };
 
-use log::warn;
+use log::{debug, warn};
+
+use crate::logger::fmt_slice_hex;
 
 use super::message;
 
-pub struct IpHeader {
-    src_addr: IpAddr,
-    dst_addr: IpAddr,
-    src_port: Option<u16>,
-    dst_port: Option<u16>,
-    transport_protocol: message::IPProtocolType,
-}
-
 #[derive(Clone, Copy, PartialEq, Eq)]
-struct Ipv6NextHeader(u8);
+pub struct TransportProtocolType(u8);
 
-impl Ipv6NextHeader {
-    const HOP_BY_HOP_OPTIONS: Ipv6NextHeader = Ipv6NextHeader(0);
-    const ROUTING: Ipv6NextHeader = Ipv6NextHeader(43);
-    const FRAGMENT: Ipv6NextHeader = Ipv6NextHeader(44);
-    const DESTINATION_OPTIONS: Ipv6NextHeader = Ipv6NextHeader(60);
-    const NO_NEXT_HEADER: Ipv6NextHeader = Ipv6NextHeader(59);
+impl TransportProtocolType {
+    const HOP_BY_HOP_OPTIONS: TransportProtocolType = TransportProtocolType(0);
+    const ICMP: TransportProtocolType = TransportProtocolType(1);
+    pub const TCP: TransportProtocolType = TransportProtocolType(6);
+    pub const UDP: TransportProtocolType = TransportProtocolType(17);
+    const IPV6_ROUTING: TransportProtocolType = TransportProtocolType(43);
+    const IPV6_FRAGMENT: TransportProtocolType = TransportProtocolType(44);
+    const IPV6_ICMP: TransportProtocolType = TransportProtocolType(58);
+    const IPV6_DESTINATION_OPTIONS: TransportProtocolType = TransportProtocolType(60);
+    const IPV6_NO_NEXT_HEADER: TransportProtocolType = TransportProtocolType(59);
 }
 
-impl Ipv6NextHeader {
-    fn length(&self, data: &[u8]) -> Option<usize> {
+impl TransportProtocolType {
+    fn from_u8(value: u8) -> TransportProtocolType {
+        TransportProtocolType(value)
+    }
+
+    fn length(&self, data: &[u8]) -> usize {
         match *self {
-            Self::HOP_BY_HOP_OPTIONS => Some(data[1] as usize + 1),
-            Self::ROUTING => Some(data[1] as usize + 1),
-            Self::FRAGMENT => Some(8),
-            Self::DESTINATION_OPTIONS => Some(data[1] as usize + 1),
-            Self::NO_NEXT_HEADER => None,
-            _ => None,
+            Self::HOP_BY_HOP_OPTIONS => data[1] as usize + 1,
+            Self::IPV6_ROUTING => data[1] as usize + 1,
+            Self::IPV6_FRAGMENT => 8,
+            Self::IPV6_DESTINATION_OPTIONS => data[1] as usize + 1,
+            Self::IPV6_NO_NEXT_HEADER => data.len(),
+            _ => data.len(),
         }
     }
 
     fn min_bytes(&self) -> usize {
         match *self {
             Self::HOP_BY_HOP_OPTIONS => 2,
-            Self::ROUTING => 2,
-            Self::FRAGMENT => 8,
-            Self::DESTINATION_OPTIONS => 2,
-            Self::NO_NEXT_HEADER => 0,
+            Self::IPV6_ROUTING => 2,
+            Self::IPV6_FRAGMENT => 8,
+            Self::IPV6_DESTINATION_OPTIONS => 2,
+            Self::IPV6_NO_NEXT_HEADER => 0,
             _ => 0,
         }
     }
 }
 
-impl IpHeader {
-    pub fn from_packet(data: &[u8]) -> Result<IpHeader, IpError> {
+impl PartialEq<message::IPProtocolType> for TransportProtocolType {
+    fn eq(&self, other: &message::IPProtocolType) -> bool {
+        let protocol_id = other.protocol_id();
+        protocol_id != 0 && self.0 == protocol_id
+    }
+}
+
+impl fmt::Display for TransportProtocolType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            Self::HOP_BY_HOP_OPTIONS => write!(f, "HOPOPT"),
+            Self::ICMP => write!(f, "ICMP"),
+            Self::TCP => write!(f, "TCP"),
+            Self::UDP => write!(f, "UDP"),
+            Self::IPV6_ROUTING => write!(f, "IPv6-Route"),
+            Self::IPV6_FRAGMENT => write!(f, "IPv6-Frag"),
+            Self::IPV6_DESTINATION_OPTIONS => write!(f, "IPv6-Opts"),
+            Self::IPV6_ICMP => write!(f, "IPv6-ICMP"),
+            Self::IPV6_NO_NEXT_HEADER => write!(f, "IPv6-NoNxt"),
+            _ => write!(f, "Unknown IP transport protocol or header type {}", self.0),
+        }
+    }
+}
+
+pub struct Ipv4Packet<'a> {
+    data: &'a [u8],
+}
+
+impl Ipv4Packet<'_> {
+    fn from_data(data: &[u8]) -> Result<Ipv4Packet, IpError> {
+        if data.len() < 20 {
+            return Err("Not enough bytes in IPv4 header".into());
+        }
+        if data.len() < Self::header_length(data) {
+            return Err("IPv4 header length overflow".into());
+        }
+        let packet = Ipv4Packet { data };
+        if packet.transport_protocol() == TransportProtocolType::from_u8(0) {
+            Err("IPv4 protocol is 0".into())
+        } else {
+            Ok(packet)
+        }
+    }
+
+    fn header_length(data: &[u8]) -> usize {
+        (data[0] & 0x0f) as usize * 4
+    }
+
+    fn src_addr(&self) -> Ipv4Addr {
+        let mut src_addr = [0u8; 4];
+        src_addr.copy_from_slice(&self.data[12..16]);
+        Ipv4Addr::from(src_addr)
+    }
+
+    fn dst_addr(&self) -> Ipv4Addr {
+        let mut dst_addr = [0u8; 4];
+        dst_addr.copy_from_slice(&self.data[16..20]);
+        Ipv4Addr::from(dst_addr)
+    }
+
+    fn transport_protocol(&self) -> TransportProtocolType {
+        TransportProtocolType::from_u8(self.data[9])
+    }
+
+    fn transport_protocol_data(&self) -> &[u8] {
+        let header_length = Self::header_length(self.data);
+        &self.data[header_length..]
+    }
+}
+
+pub struct Ipv6Packet<'a> {
+    data: &'a [u8],
+    payload_type: TransportProtocolType,
+    payload_data: &'a [u8],
+}
+
+impl Ipv6Packet<'_> {
+    fn from_data(data: &[u8]) -> Result<Ipv6Packet, IpError> {
+        if data.len() < 40 {
+            return Err("Not enough bytes in IPv6 header".into());
+        }
+        let last_payload = if let Some(last_payload) = Self::iter_payloads(data).last() {
+            last_payload?
+        } else {
+            return Err("IPv6 packet has no payload".into());
+        };
+
+        Ok(Ipv6Packet {
+            data,
+            payload_type: last_payload.protocol_type,
+            payload_data: last_payload.data,
+        })
+    }
+
+    fn iter_payloads(data: &[u8]) -> Ipv6PacketIter {
+        Ipv6PacketIter {
+            next_payload: TransportProtocolType(data[6]),
+            data: &data[40..],
+        }
+    }
+
+    fn src_addr(&self) -> Ipv6Addr {
+        let mut src_addr = [0u8; 16];
+        src_addr.copy_from_slice(&self.data[8..24]);
+        Ipv6Addr::from(src_addr)
+    }
+
+    fn dst_addr(&self) -> Ipv6Addr {
+        let mut dst_addr = [0u8; 16];
+        dst_addr.copy_from_slice(&self.data[24..40]);
+        Ipv6Addr::from(dst_addr)
+    }
+
+    fn transport_protocol(&self) -> TransportProtocolType {
+        self.payload_type
+    }
+
+    fn transport_protocol_data(&self) -> &[u8] {
+        self.payload_data
+    }
+}
+
+pub struct Ipv6PayloadHeader<'a> {
+    protocol_type: TransportProtocolType,
+    data: &'a [u8],
+}
+
+pub struct Ipv6PacketIter<'a> {
+    next_payload: TransportProtocolType,
+    data: &'a [u8],
+}
+
+impl<'a> Iterator for Ipv6PacketIter<'a> {
+    type Item = Result<Ipv6PayloadHeader<'a>, IpError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.data.is_empty() {
+            return None;
+        }
+        if self.data.len() < self.next_payload.min_bytes() {
+            self.data = &[];
+            return Some(Err("IPv6 header minimal length overlow".into()));
+        }
+        let header_length = self.next_payload.length(self.data);
+        if self.data.len() < header_length {
+            self.data = &[];
+            return Some(Err("IPv6 header length overlow".into()));
+        }
+        let data = self.data;
+        let protocol_type = self.next_payload;
+        self.next_payload = TransportProtocolType(data[0]);
+        self.data = &self.data[header_length..];
+        Some(Ok(Ipv6PayloadHeader {
+            protocol_type,
+            data: &data[..header_length],
+        }))
+    }
+}
+
+pub enum IpPacket<'a> {
+    V4(Ipv4Packet<'a>),
+    V6(Ipv6Packet<'a>),
+}
+
+impl IpPacket<'_> {
+    pub fn from_data(data: &[u8]) -> Result<IpPacket, IpError> {
         if data.is_empty() {
             return Err("IP packet is empty, cannot extract header data".into());
         }
         match data[0] >> 4 {
-            4 => Self::from_ipv4_packet(data),
-            6 => Self::from_ipv6_packet(data),
+            4 => Ok(IpPacket::V4(Ipv4Packet::from_data(data)?)),
+            6 => Ok(IpPacket::V6(Ipv6Packet::from_data(data)?)),
             _ => {
                 warn!("ESP IP packet is not a supported IP version: {:x}", data[0]);
                 Err("Unsupported IP prococol version".into())
@@ -66,6 +232,86 @@ impl IpHeader {
         }
     }
 
+    pub fn src_addr(&self) -> IpAddr {
+        match self {
+            IpPacket::V4(packet) => IpAddr::V4(packet.src_addr()),
+            IpPacket::V6(packet) => IpAddr::V6(packet.src_addr()),
+        }
+    }
+
+    pub fn dst_addr(&self) -> IpAddr {
+        match self {
+            IpPacket::V4(packet) => IpAddr::V4(packet.dst_addr()),
+            IpPacket::V6(packet) => IpAddr::V6(packet.dst_addr()),
+        }
+    }
+
+    pub fn transport_protocol(&self) -> TransportProtocolType {
+        match self {
+            IpPacket::V4(packet) => packet.transport_protocol(),
+            IpPacket::V6(packet) => packet.transport_protocol(),
+        }
+    }
+
+    pub fn transport_protocol_data(&self) -> &[u8] {
+        match self {
+            IpPacket::V4(packet) => packet.transport_protocol_data(),
+            IpPacket::V6(packet) => packet.transport_protocol_data(),
+        }
+    }
+
+    pub fn src_port(&self) -> Result<Option<u16>, IpError> {
+        match self.transport_protocol() {
+            TransportProtocolType::TCP | TransportProtocolType::UDP => {
+                let data = self.transport_protocol_data();
+                if data.len() < 2 {
+                    return Err("Not enough data in transport layer to extract source port".into());
+                }
+                let mut src_port = [0u8; 2];
+                src_port.copy_from_slice(&data[0..2]);
+                Ok(Some(u16::from_be_bytes(src_port)))
+            }
+            _ => Ok(None),
+        }
+    }
+
+    pub fn dst_port(&self) -> Result<Option<u16>, IpError> {
+        match self.transport_protocol() {
+            TransportProtocolType::TCP | TransportProtocolType::UDP => {
+                let data = self.transport_protocol_data();
+                if data.len() < 4 {
+                    return Err(
+                        "Not enough data in transport layer to extract destination port".into(),
+                    );
+                }
+                let mut dst_port = [0u8; 2];
+                dst_port.copy_from_slice(&data[2..4]);
+                Ok(Some(u16::from_be_bytes(dst_port)))
+            }
+            _ => Ok(None),
+        }
+    }
+
+    pub fn to_header(&self) -> Result<IpHeader, IpError> {
+        Ok(IpHeader {
+            src_addr: self.src_addr(),
+            dst_addr: self.dst_addr(),
+            src_port: self.src_port()?,
+            dst_port: self.dst_port()?,
+            transport_protocol: self.transport_protocol(),
+        })
+    }
+}
+
+pub struct IpHeader {
+    src_addr: IpAddr,
+    dst_addr: IpAddr,
+    src_port: Option<u16>,
+    dst_port: Option<u16>,
+    transport_protocol: TransportProtocolType,
+}
+
+impl IpHeader {
     #[inline]
     pub fn src_addr(&self) -> &IpAddr {
         &self.src_addr
@@ -87,93 +333,27 @@ impl IpHeader {
     }
 
     #[inline]
-    pub fn transport_protocol(&self) -> message::IPProtocolType {
+    pub fn transport_protocol(&self) -> TransportProtocolType {
         self.transport_protocol
     }
+}
 
-    fn from_ipv4_packet(data: &[u8]) -> Result<IpHeader, IpError> {
-        if data.len() < 20 {
-            return Err("Not enough bytes in IPv4 header".into());
+impl fmt::Display for IpHeader {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.src_port {
+            Some(src_port) => write!(
+                f,
+                "{} {}:{} -> ",
+                self.transport_protocol(),
+                self.src_addr(),
+                src_port
+            )?,
+            None => write!(f, "{} {} -> ", self.transport_protocol(), self.src_addr())?,
         }
-        let header_length = (data[0] & 0x0f) as usize * 4;
-        if data.len() < header_length {
-            return Err("IPv4 header length overflow".into());
+        match self.dst_port {
+            Some(dst_port) => writeln!(f, "{}:{}", self.dst_addr(), dst_port),
+            None => writeln!(f, "{}", self.dst_addr()),
         }
-        let transport_protocol = message::IPProtocolType::from_u8(data[9]);
-        let (src_port, dst_port) = match transport_protocol {
-            message::IPProtocolType::TCP | message::IPProtocolType::UDP => {
-                Self::extract_ports(&data[header_length..])?
-            }
-            message::IPProtocolType::ANY => return Err("IPv4 protocol is 0".into()),
-            _ => (None, None),
-        };
-        let mut src_addr = [0u8; 4];
-        src_addr.copy_from_slice(&data[12..16]);
-        let src_addr = IpAddr::V4(Ipv4Addr::from(src_addr));
-        let mut dst_addr = [0u8; 4];
-        dst_addr.copy_from_slice(&data[16..20]);
-        let dst_addr = IpAddr::V4(Ipv4Addr::from(dst_addr));
-        Ok(IpHeader {
-            src_addr,
-            dst_addr,
-            src_port,
-            dst_port,
-            transport_protocol,
-        })
-    }
-
-    fn from_ipv6_packet(data: &[u8]) -> Result<IpHeader, IpError> {
-        if data.len() < 40 {
-            return Err("Not enough bytes in IPv6 header".into());
-        }
-        // TODO: test that this works.
-        let mut next_header = Ipv6NextHeader(data[6]);
-        let mut next_header_start = 40;
-        loop {
-            if next_header_start + next_header.min_bytes() > data.len() {
-                return Err("IPv6 header length overlow".into());
-            }
-            if let Some(header_length) = next_header.length(&data[next_header_start..]) {
-                next_header = Ipv6NextHeader(data[next_header_start]);
-                next_header_start += header_length;
-            } else {
-                break;
-            }
-        }
-        let transport_protocol = message::IPProtocolType::from_u8(next_header.0);
-        let (src_port, dst_port) = match transport_protocol {
-            message::IPProtocolType::TCP | message::IPProtocolType::UDP => {
-                Self::extract_ports(&data[next_header_start..])?
-            }
-            message::IPProtocolType::ANY => return Err("IPv4 protocol is 0".into()),
-            _ => (None, None),
-        };
-        let mut src_addr = [0u8; 16];
-        src_addr.copy_from_slice(&data[8..24]);
-        let src_addr = IpAddr::V6(Ipv6Addr::from(src_addr));
-        let mut dst_addr = [0u8; 16];
-        dst_addr.copy_from_slice(&data[24..40]);
-        let dst_addr = IpAddr::V6(Ipv6Addr::from(dst_addr));
-        Ok(IpHeader {
-            src_addr,
-            dst_addr,
-            src_port,
-            dst_port,
-            transport_protocol,
-        })
-    }
-
-    fn extract_ports(data: &[u8]) -> Result<(Option<u16>, Option<u16>), IpError> {
-        if data.len() < 4 {
-            return Err("Not enough data in transport layer to extract ports".into());
-        }
-        let mut src_port = [0u8; 2];
-        src_port.copy_from_slice(&data[0..2]);
-        let src_port = u16::from_be_bytes(src_port);
-        let mut dst_port = [0u8; 2];
-        dst_port.copy_from_slice(&data[2..4]);
-        let dst_port = u16::from_be_bytes(dst_port);
-        Ok((Some(src_port), Some(dst_port)))
     }
 }
 
@@ -351,23 +531,39 @@ impl Network {
     }
 }
 
-impl fmt::Display for IpHeader {
+impl fmt::Display for IpPacket<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if let Some(src_port) = self.src_port {
-            write!(
+        match self {
+            IpPacket::V4(_) => write!(f, "IPv4 ")?,
+            IpPacket::V6(_) => write!(f, "IPv6 ")?,
+        }
+        match self.src_port() {
+            Ok(Some(src_port)) => write!(
                 f,
                 "{} {}:{} -> ",
-                self.transport_protocol, self.src_addr, src_port
-            )?;
-        } else {
-            write!(f, "{} {} -> ", self.transport_protocol, self.src_addr)?;
+                self.transport_protocol(),
+                self.src_addr(),
+                src_port
+            )?,
+            Ok(None) => write!(f, "{} {} -> ", self.transport_protocol(), self.src_addr())?,
+            Err(err) => write!(
+                f,
+                "{} {} (port {}) -> ",
+                self.transport_protocol(),
+                self.src_addr(),
+                err
+            )?,
         }
-        if let Some(dst_port) = self.dst_port {
-            write!(f, "{}:{}", self.dst_addr, dst_port)?;
-        } else {
-            write!(f, "{}", self.dst_addr)?;
+        match self.dst_port() {
+            Ok(Some(dst_port)) => writeln!(f, "{}:{}", self.dst_addr(), dst_port)?,
+            Ok(None) => writeln!(f, "{}", self.dst_addr())?,
+            Err(err) => writeln!(f, "{} (port {}) -> ", self.dst_addr(), err)?,
         }
-        Ok(())
+        writeln!(
+            f,
+            "  Transport {}",
+            fmt_slice_hex(self.transport_protocol_data())
+        )
     }
 }
 
