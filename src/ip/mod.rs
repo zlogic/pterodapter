0075bace59a29a0fca745415320bbbbee5dfd1ce
Dmitrii Zolotukhin <dz@3d-net.nl>
2026-02-20 18:11:51 +0100

    Removed debug code.
    It appears that the issue is caused by macOS not properly setting TCP
    checksums.

--- src/ip/mod.rs
+++ src/ip/mod.rs
@@ -1222,14 +1222,14 @@ impl<'a> IpPacket<'a> {
         }
     }
 
-    pub fn validate_ip_checksum(&self) -> bool {
+    fn validate_ip_checksum(&self) -> bool {
         match self {
             IpPacket::V4(packet) => packet.validate_ip_checksum(),
             IpPacket::V6(_) => true,
         }
     }
 
-    pub fn validate_transport_checksum(&self) -> bool {
+    fn validate_transport_checksum(&self) -> bool {
         match self {
             IpPacket::V4(packet) => packet.validate_transport_checksum(),
             IpPacket::V6(packet) => packet.validate_transport_checksum(),
