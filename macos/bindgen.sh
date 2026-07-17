09d482bb028cf652f22145b23694c7720daefb72
Dmitrii Zolotukhin <dz@3d-net.nl>
2026-07-17 19:55:11 +0200

    Revert "Attempting to apply additional network options."
    This reverts commit c30ae5f31fa322ead4989c1f43bd447e75568660.

--- macos/bindgen.sh
+++ macos/bindgen.sh
@@ -6,17 +6,7 @@ INCLUDE_FILE="$(mktemp -u).h"
 echo '#include <vmnet/vmnet.h>' > $INCLUDE_FILE
 xcrun bindgen \
   --no-derive-copy --no-derive-debug\
-  --allowlist-function vmnet_network_configuration_create\
-  --allowlist-function vmnet_network_configuration_disable_nat44\
-  --allowlist-function vmnet_network_configuration_disable_nat66\
-  --allowlist-function vmnet_network_configuration_disable_dhcp\
-  --allowlist-function vmnet_network_configuration_disable_dns_proxy\
-  --allowlist-function vmnet_network_configuration_disable_router_advertisement\
-  --allowlist-function vmnet_network_configuration_set_mtu\
-  --allowlist-function vmnet_network_configuration_set_ipv6_prefix\
-  --allowlist-function vmnet_network_configuration_set_ipv4_subnet\
-  --allowlist-function vmnet_network_create\
-  --allowlist-function vmnet_interface_start_with_network\
+  --allowlist-function vmnet_start_interface\
   --allowlist-function vmnet_stop_interface\
   --allowlist-function vmnet_interface_set_event_callback\
   --allowlist-function vmnet_read\
