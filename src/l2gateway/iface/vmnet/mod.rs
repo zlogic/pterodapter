c175c688efdd46154030b9fa508e98ae00cc1112
Dmitrii Zolotukhin <dz@3d-net.nl>
2026-09-15 18:48:29 +0200

    Experiment: log object from vmnet_start_interface.
    Nothing useful here to detect which interface was created.

--- src/l2gateway/iface/vmnet/mod.rs
+++ src/l2gateway/iface/vmnet/mod.rs
@@ -1,4 +1,4 @@
-use std::{error, fmt, net::Ipv6Addr, task::Poll};
+use std::{error, ffi::CStr, fmt, net::Ipv6Addr, task::Poll};
 
 use log::{debug, trace, warn};
 use rand::Rng as _;
@@ -105,7 +105,12 @@ impl Vmnet {
 
         let (tx, mut rx) = mpsc::channel(1);
         let block = block2::RcBlock::new(
-            move |status: sys::vmnet_return_t, _params: sys::xpc_object_t| {
+            move |status: sys::vmnet_return_t, params: sys::xpc_object_t| {
+                unsafe {
+                    let params =
+                        CStr::from_ptr(sys::xpc_copy_description(params)).to_string_lossy();
+                    println!("Got params object: {params}");
+                }
                 let _ = tx.blocking_send(VmnetResponse::from_code(status));
             },
         );
